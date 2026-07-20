bcff25d83207a6706a503e751b4f86f37fa9718f
sirdrick037 <sirdrick 037@gmail.com>
2026-07-20 11:14:17 +0300

    Change userForm

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById("userform");
+const form = document.getElementById("userForm");
 const nameInput = document.getElementById("name");
 const ageInput = document.getElementById("age");
 
